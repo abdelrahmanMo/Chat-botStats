9465e127a78f7416c0950c630d24d7c76149018e
abdelrahmanMo <abdo.mohamed.am.ao@gmail.com>
2020-10-15 17:40:16 +0200

    Add project files.

---
+++ Dxwand/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dxwand.MvcApplication" Language="C#" %>
---
+++ Dxwand/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dxwand.Startup))]
+namespace Dxwand
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
